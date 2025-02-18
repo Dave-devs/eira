44fd3cd85fe29aaaa5505d3bc50a8ebc29303b42
Dave-devs <Odetundeoreoluwadavid@gmail.com>
2025-02-19 00:46:02 +0100

    refactor (Title): update title component styling

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -9,7 +9,7 @@ export default function Title({ text1, text2 }: TitleProps) {
   return (
     <div data-testid="title-container" className='inline-flex items-center gap-2 mb-3 font-playfair'>
       <p data-testid="title-text" className='text-gray-500 dark:text-gray-400'>
-        {text1}
+        {text1}{" "}
         <span data-testid="title-span" className='text-gray-700 dark:text-gray-50 font-medium'>
           {text2}
         </span>
